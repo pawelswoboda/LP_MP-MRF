cb6c8902a339caf81003bfd5b870cf2a4aacb6f7
Paul SWOBODA <paul.swoboda@ist.ac.at>
2017-11-27 15:18:10 +0100

    Initial code commit.

---
+++ test/simplex_marginalization.cpp
@@ -0,0 +1,73 @@
+#include "test.h"
+#include "simplex_factor.hxx"
+#include "simplex_marginalization_message.hxx"
+#include <vector>
+
+using namespace LP_MP;
+
+int main()
+{
+  // "unary/pairwise message", "[unary/pairwise message between simplex factors]" 
+   std::vector<double> costUnary {0.1, 0.2, 0.05, 1};
+   std::vector<double> costPairwise {  0.1,    0.2,  0.05,
+                                  0.3,  0.001,  0.2 ,
+                                 -0.3, -0.001, -0.2 ,
+                                  0.3,  0.001,  0.2 };
+   UnarySimplexFactor simplexUnary(costUnary);
+   PairwiseSimplexFactor simplexPairwise(4,3);
+   for(INDEX i=0; i<4; ++i) {
+      for(INDEX j=0; j<3; ++j) {
+         simplexPairwise.cost(i,j) = costPairwise[i*3 + j];
+      }
+   }
+
+   UnaryPairwiseMessage<Chirality::left,true> leftMessage(4,3);
+   UnaryPairwiseMessage<Chirality::right,true> rightMessage(4,3);
+
+   // must add operators -= and += to vector to support the below things
+   // "marginalize pairwise right"
+   {
+     vector<REAL> marg(4,0.0);
+     leftMessage.send_message_to_left(simplexPairwise, marg);
+     test(marg[0] == -0.05);
+     test(marg[1] == -0.001);
+     test(marg[2] ==  0.3);
+     test(marg[3] == -0.001);
+   }
+
+   // "marginalize pairwise left"
+   {
+     vector<REAL> marg(3,0.0);
+     rightMessage.send_message_to_left(simplexPairwise, marg);
+     test(marg[0] == 0.3);
+     test(marg[1] == 0.001);
+     test(marg[2] == 0.2);
+   }
+
+   // do zrobienia: test ComputeLeftFromRightPrimal and reverse, repamLeft,repamRight, Send{Left|Right}Message and also interchange unary and right/left loop
+
+}
+
+/*
+TEST_CASE("pairwise/triplet message", "[pairwise/triplet message between simplex factors]") {
+   PairwiseSimplexFactor simplexPairwise(4,3);
+   for(INDEX i=0; i<3; ++i) {
+      for(INDEX j=0; j<3; ++j) {
+         simplexPairwise(i,j) = costPairwise[i*3 + j];
+      }
+   }
+   SimpleTighteningTernarySimplexFactor simplexTriplet(3,3,3);
+
+   PairwiseTripletMessage12<MessageSendingType::SRMP> message(3,3,3);
+
+   SECTION( "marginalize triplet" ) {
+      matrix marg(3,3,0.0);
+      message.ReceiveMessageFromRight(simplexTriplet, marg);
+   }
+
+   SECTION( "marginalize pairwise" ) {
+      matrix marg(3,3,0.0);
+      message.ReceiveMessageFromLeft(simplexPairwise, marg);
+   }
+}
+*/
